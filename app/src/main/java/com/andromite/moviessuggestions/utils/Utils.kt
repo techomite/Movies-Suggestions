0d1cab13ddbb91d5ea91583187eac77f0efbb534
Mohammed Faizan Hussain <faizan@brochill.com>
2022-08-22 16:30:46 +0530

    [code] keyboard utils

--- app/src/main/java/com/andromite/moviessuggestions/utils/Utils.kt
+++ app/src/main/java/com/andromite/moviessuggestions/utils/Utils.kt
@@ -1,6 +1,10 @@
 package com.andromite.moviessuggestions.utils
 
+import android.app.Activity
 import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
 
 class Utils {
 
@@ -13,6 +17,18 @@ class Utils {
         fun floge(value: Any) {
             if (Constants.ENABLE_LOGS)
                 Log.e("asdfasdf", ":-: $value :-:")
+        }
+
+        fun hideKeyboard(activity: Activity) {
+            val imm: InputMethodManager =
+                activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            //Find the currently focused view, so we can grab the correct window token from it.
+            var view: View? = activity.currentFocus
+            //If no view currently has focus, create a new one, just so we can grab a window token from it
+            if (view == null) {
+                view = View(activity)
+            }
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0)
         }
     }
 }
